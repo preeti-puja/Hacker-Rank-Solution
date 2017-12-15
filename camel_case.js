4f25b2fcc568d5dc185c4f62227d158adc086187
preeti-puja <preeti.puja18@gmail.com>
2017-12-15 13:58:01 +0530

    solution to camel case problem

---
+++ camel_case.js
@@ -0,0 +1,54 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+  var s = readLine();
+  var i = 0, word = 1;
+  for (i = 0; i < s.length; i++ ){
+    if (s[i] == s[i].toUpperCase() ){
+      word++
+    }
+  }
+  console.log(word);
+}
